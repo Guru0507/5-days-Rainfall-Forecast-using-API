e60aadfba06950be0da6ebd9f5713219a78f979c
Guruprasad Deshpande <64199999+Guru0507@users.noreply.github.com>
2022-06-15 12:20:28 +0530

    Add files via upload

---
+++ Current rainfall.py
@@ -0,0 +1,23 @@
+import requests
+from pprint import pprint
+
+city = input('Enter your city : ')
+
+url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=37746f16507a86de96c3343089d7e831&units=metric'.format(city)
+
+res = requests.get(url)
+
+data = res.json()
+
+temp = data['main']['temp']
+wind_speed = data['wind']['speed']
+latitude = data['coord']['lat']
+longitude = data['coord']['lon']
+
+description = data['weather'][0]['description']
+
+print('Temperature : {} degree celcius'.format(temp))
+print('Wind Speed : {} m/s'.format(wind_speed))
+print('Latitude : {}'.format(latitude))
+print('Longitude : {}'.format(longitude))
+print('Description : {}'.format(description))
